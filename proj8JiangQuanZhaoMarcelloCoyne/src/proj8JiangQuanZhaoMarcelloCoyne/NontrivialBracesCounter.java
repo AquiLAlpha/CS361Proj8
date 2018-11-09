e795e81efff1e59ebd6d59ce854262e2d28ef2eb
Liwei Jiang <ljiang@colby.edu>
2018-11-09 02:07:04 -0500

    add comments to the second version of the code

--- proj8JiangQuanZhaoMarcelloCoyne/src/proj8JiangQuanZhaoMarcelloCoyne/NontrivialBracesCounter.java
+++ proj8JiangQuanZhaoMarcelloCoyne/src/proj8JiangQuanZhaoMarcelloCoyne/NontrivialBracesCounter.java
@@ -3,8 +3,7 @@
  * F18 CS361 Project 8
  * Names: Liwei Jiang, Tracy Quan, Danqing Zhao, Chris Marcello, Michael Coyne
  * Date: 11/06/2018
- * This file contains the NontrivialBracesCounter class,
- * analyzing a Java source file and finding all non-trivial left braces '{'.
+ * Contains the NontrivialBracesCounter class, analyzing a Java source file and finding all non-trivial left braces '{'.
  */
 
 package proj8JiangQuanZhaoMarcelloCoyne;
@@ -15,45 +14,71 @@ import java.lang.*;
  * NontrivialBracesCounter class, counting the number of non-trivial left braces in a legal Java program.
  */
 public class NontrivialBracesCounter {
-    private boolean ifStringSingle = false;
+    /**
+     * Boolean flags indicating whether scanning within a single quote string,
+     * a double quote string, a comment, or a block comment.
+     */
     private boolean ifStringDouble = false;
+    private boolean ifStringSingle = false;
     private boolean ifComment = false;
     private boolean ifBlockComment = false;
 
-
-    private int countNumLeftBracesInString(String source) {
+    /**
+     * Helper method to count the number of non-trivial left braces in a string.
+     *
+     * @param source a string to be examined for the number of non-trivial left braces
+     * @return the count of the number of non-trivial left braces in the given string
+     */
+    private int countNumNontrivialLeftBracesInString(String source) {
+        // extend the beginning and the end of the string to avoid out of bound error
         source = " " + source + " ";
         int count = 0;
 
-        this.checkStartIndicator(source, 0);
+        this.checkTrivialBeginIndicator(source, 0);
         for (int i = 1; i < source.length() - 1; i++) {
-            if (!this.ifStringSingle && !this.ifStringDouble && !this.ifComment && !this.ifBlockComment) {
+            // if the current character is in a non-trivial block
+            // check if the current character is a left brace
+            // or otherwise check whether the current character begins a trivial block (quote or comment)
+            if (!this.ifStringDouble && !this.ifStringSingle && !this.ifComment && !this.ifBlockComment) {
                 if (source.charAt(i) == '{') count++;
-                this.checkStartIndicator(source, i);
-            } else {
-                this.checkEndIndicator(source, i);
+                else this.checkTrivialBeginIndicator(source, i);
+            }
+            // if the current character is in a trivial block (quote or comment)
+            // then check whether the current character ends the trivial block
+            else {
+                this.checkTrivialEndIndicator(source, i);
             }
         }
-
         return count;
     }
 
-
-    private void checkStartIndicator(String source, int index) {
+    /**
+     * Helper method to check whether a specified character is a begin indicator of a trivial block.
+     * The begin indicators include ", ', //, /* for single quote, double quote, comment, and block comment.
+     *
+     * @param source a source string
+     * @param index the index of the specified character
+     */
+    private void checkTrivialBeginIndicator(String source, int index) {
         if (source.charAt(index) == '\"') this.ifStringDouble = true;
         else if (source.charAt(index) == '\'') this.ifStringSingle = true;
         else if (source.charAt(index) == '/' && source.charAt(index + 1) == '/') this.ifComment = true;
         else if (source.charAt(index) == '/' && source.charAt(index + 1) == '*') this.ifBlockComment = true;
     }
 
-
-    private void checkEndIndicator(String source, int index) {
+    /**
+     * Helper method to check whether a specified character is a end indicator of a trivial block.
+     * The end indicators include ", ', \n, * / for single quote, double quote, comment, and block comment.
+     *
+     * @param source a source string
+     * @param index the index of the specified character
+     */
+    private void checkTrivialEndIndicator(String source, int index) {
         if (source.charAt(index) == '\"' && this.ifStringDouble) this.ifStringDouble = false;
         else if (source.charAt(index) == '\'' && this.ifStringSingle) this.ifStringSingle = false;
         else if (source.charAt(index) == '\n' && this.ifComment) this.ifComment = false;
         else if (source.charAt(index) == '*' && source.charAt(index + 1) == '/' && this.ifBlockComment) this.ifBlockComment = false;
     }
-
 
     /**
      * Helper method to get the content of the specified file.
@@ -87,12 +112,12 @@ public class NontrivialBracesCounter {
      *         -1 if exception occurs when opening the file
      */
     public int getNumNontrivialLeftBraces(String fileName) {
-            String fileContent = this.getFileContent(fileName);
-            // if the file opens successfully
-            if (fileContent != null) {
-                return(this.countNumLeftBracesInString(fileContent));
-            }
-            return(-1);
+        String fileContent = this.getFileContent(fileName);
+        // if the file opens successfully
+        if (fileContent != null) {
+            return(this.countNumNontrivialLeftBracesInString(fileContent));
+        }
+        return(-1);
     }
 
     /**
